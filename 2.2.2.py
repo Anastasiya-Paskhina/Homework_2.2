e2b3c556a1e46b549a1905ea229537c77c4a8f79
Anastasiya-Paskhina <45151607+Anastasiya-Paskhina@users.noreply.github.com>
2019-01-28 22:17:03 +0200

    Update 2.2.2.py

--- 2.2.2.py
+++ 2.2.2.py
@@ -1,12 +1,12 @@
 import hashlib
 
 
-def generate(file):
+def generator(file):
     with open(file) as f:
         for line in f:
             h = hashlib.md5(line.encode('utf-8'))
             yield h.hexdigest()
 
 
-for item in generate('C:\\Users\\Александр\\PycharmProjects\\Homework 2.2\\output.txt'):
+for item in generator('C:\\Users\\Александр\\PycharmProjects\\Homework 2.2\\output.txt'):
     print(item)
